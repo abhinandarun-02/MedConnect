151c5cfddc660a9aa8c81bef821646b341a15073
Abhinand Arun <abhinandarun2015@gmail.com>
2023-08-31 02:20:49 +0530

    chore: update user.name

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { ModeToggle } from '@/components/theme-button'
 import { UserButton, auth } from '@clerk/nextjs'
 
 export default function Home() {
-  const { userId } = auth()
+  const { user } = auth()
   return (
     <main className="flex items-center justify-between bg-slate-400 px-8 py-2  dark:bg-slate-800">
-      <h1>{'hello ' + userId}</h1>
+      <h1>{'hello ' + (user?.firstName || 'John')}</h1>
       <div className="flex gap-4">
         <ModeToggle />
         <UserButton afterSignOutUrl="/" />
