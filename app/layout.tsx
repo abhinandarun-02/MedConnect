fe922c92df6bcffbcb9b98b9c14143e644cc286e
Abhinand Arun <abhinandarun2015@gmail.com>
2023-08-31 01:49:58 +0530

    feat: Add App logo and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'MedConnect',
   description: "Connect with the world's best doctors",
+
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({
