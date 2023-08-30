ea6d6353f02676d78d5a1d978582d94fc6e6990a
Abhinand Arun <abhinandarun2015@gmail.com>
2023-08-31 01:51:16 +0530

    feat: Setup darkmode with next/themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '@/components/theme-button'
+import { UserButton, auth } from '@clerk/nextjs'
 
 export default function Home() {
+  const { userId } = auth()
   return (
-    <main>
-      Hello
-      <UserButton afterSignOutUrl="/" />
+    <main className="flex items-center justify-between bg-slate-400 px-8 py-2  dark:bg-slate-800">
+      <h1>{'hello ' + userId}</h1>
+      <div className="flex gap-4">
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </main>
   )
 }
